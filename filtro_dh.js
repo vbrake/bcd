535be7dc8fbf07a912181cce768b31f39585301d
vbrake <vbrake@gmail.com>
2016-12-02 14:14:34 -0200

    Create filtro_dh.js

---
+++ filtro_dh.js
@@ -0,0 +1,10 @@
+//instalar com 'npm install json-sql' ou adicionar ao package.json
+
+//exemplo de criação de sql query
+var sql = jsonSql.build({
+    type: 'select',
+    table: 'filtro_disciplinas_horario'
+});
+
+sql.query
+// select * from "table";
