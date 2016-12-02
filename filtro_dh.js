58b827c34ec1d74ab075e155b13af791be51af3e
vbrake <vbrake@gmail.com>
2016-12-02 14:47:43 -0200

    Update filtro_dh.js

--- filtro_dh.js
+++ filtro_dh.js
@@ -1,10 +1,28 @@
 //instalar com 'npm install json-sql' ou adicionar ao package.json
 
-//exemplo de criação de sql query
-var sql = jsonSql.build({
+//exemplo de sql build
+var sqlbuild = jsonSql.build({
     type: 'select',
     table: 'filtro_disciplinas_horario'
 });
 
-sql.query
+sqlbuild.query
 // select * from "filtro_disciplinas_horario";
+
+//exemplo de sql insert
+var sqlinsert = jsonSql.build({
+    type: 'insert',
+    table: 'users',
+    values: {
+        name: 'John',
+        lastname: 'Snow',
+        age: 24,
+        gender: 'male'
+    }
+});
+
+sqlinsert.query
+// insert into users (name, lastname, age, gender) values ($p1, $p2, 24, $p3);
+
+sqlinsert.values
+// { p1: 'John', p2: 'Snow', p3: 'male' }
